e7794cbf0c7ae0a9f7675bdea9f95d7010110030
jakey-1 <jakehadady1@gmail.com>
2023-11-13 18:18:07 +0300

    Dart Phone and Email Pattern

--- main.dart
+++ main.dart
@@ -1,8 +1,10 @@
 void main() {
   String patternEmail = r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$';
-  String patternPhone = r'^\+\d{1,3}\s\d{9,12}$';
   RegExp regExp = new RegExp(patternEmail);
+
+  String patternPhone = r'^\+\d{1,3}\s\d{9,12}$';
   RegExp regExp1 = new RegExp(patternPhone);
+
   String email = "omar@gmail.com";
   String phone = "+132 906564469";
   print(regExp1.hasMatch(phone));
